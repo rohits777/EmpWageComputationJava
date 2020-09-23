c403ba627ddc5bdd9a8528d2644990624f54f718
rohits777 <rohitashinde95@gmail.com>
2020-09-23 14:59:04 +0530

    Added: Employee attendance is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,15 @@
 class EmpWage {
+  public static void main(String[] args) {
+     System.out.println("***Welcome to employee wage computation Program***"); 
+       int PRESENT = 1;
+          int random=(int)Math.floor(Math.random()*10)%2;
 
- public static void main(String[] args) {
-        System.out.println("***Welcome to employee wage computation Program***"); 
+                if(PRESENT==random)
+                        System.out.println("Employee Is Present ");
+                else
+                        System.out.println("Employee Is Absent ");
+
+
+
   }
 }
