ae625b3636ec0601fcf939d43af0f4a0976781b0
rohits777 <rohitashinde95@gmail.com>
2020-09-23 15:45:43 +0530

    Added: Employee Daily Wage is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,15 +1,20 @@
 class EmpWage {
   public static void main(String[] args) {
      System.out.println("***Welcome to employee wage computation Program***"); 
-       int PRESENT = 1;
+       int present = 1, wagePerHr=20;
+        int workingHrs=0,dailyWage;
           int random=(int)Math.floor(Math.random()*10)%2;
 
-                if(PRESENT==random)
-                        System.out.println("Employee Is Present ");
-                else
-                        System.out.println("Employee Is Absent ");
+     if(present==random) 
+      {
+           System.out.println("Employee Is Present ");
+           workingHrs=8;
+ }
+      else
+            System.out.println("Employee Is Absent ");
 
+  dailyWage = wagePerHr*workingHrs; 
+        System.out.println(" Daily wage of Employee is " +dailyWage);
 
-
-  }
+    }
 }
