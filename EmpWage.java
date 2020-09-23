19c76e82976e8e1ef02ace7c98dd2838ee0dcaca
rohits777 <rohitashinde95@gmail.com>
2020-09-23 16:06:45 +0530

    Added: Part time Employee wage is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,15 +1,20 @@
 class EmpWage {
   public static void main(String[] args) {
      System.out.println("***Welcome to employee wage computation Program***"); 
-       int present = 1, wagePerHr=20;
+       int fullTime = 1,partTime=2,wagePerHr=20;
         int workingHrs=0,dailyWage;
           int random=(int)Math.floor(Math.random()*10)%2;
 
-     if(present==random) 
+     if(fullTime==random) 
       {
-           System.out.println("Employee Is Present ");
+           System.out.println("Employee Is Present full time ");
            workingHrs=8;
- }
+       }
+     else if(random==partTime)
+       {
+           System.out.println("Employee Is present  part time .");
+                        workingHrs=4;
+      }
       else
             System.out.println("Employee Is Absent ");
 
