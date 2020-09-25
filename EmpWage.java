61cdc1c942d6b265ca0fc94fd28cffbee688998c
rohits777 <rohitashinde95@gmail.com>
2020-09-26 01:23:13 +0530

    Added:wage for each company using instance variable

--- EmpWage.java
+++ EmpWage.java
@@ -1,17 +1,30 @@
 class EmpWage {
 public static final int IS_FULL_TIME=1,IS_PART_TIME=2;
-static final int MAX_WORKING_HRS=100;
-  public void computeWage ( String company,int wagePerHr,int maxHrsPerMonth,int workingDays )
+        private final String company;
+        private final int wagePerHr;
+        private final int workingDays;
+        private final int maxHrsPerMonth;
+        private int monthlyWage;
+
+
+  public EmpWage (String company,int wagePerHr,int maxHrsPerMonth,int workingDays )
   {
-
-
-     int workingHrs=0,dailyWage,monthlyWage=0,totalWorkingHrs=0,totalWorkingDays=0;
-      while (totalWorkingDays <= workingDays && totalWorkingHrs <= MAX_WORKING_HRS)
-      {
-              int random=(int)Math.floor(Math.random()*10)%3;
+                this.company=company;
+                this.wagePerHr=wagePerHr;
+                this.workingDays=workingDays;
+                this.maxHrsPerMonth=maxHrsPerMonth;
+  }
+  public void computeEmpWage()
+  {
+     int workingHrs=0,dailyWage,monthlyWage,totalWorkingHrs=0,totalWorkingDays=0;
+      while (totalWorkingDays < workingDays && totalWorkingHrs <= maxHrsPerMonth)
+       {
               totalWorkingDays++;
-         switch(random) 
-         {
+               int random=(int)Math.floor(Math.random()*10)%3;
+
+          System.out.println(random);
+          switch (random)
+     {
            case IS_FULL_TIME:
 
                  workingHrs=8;
@@ -30,18 +43,22 @@ static final int MAX_WORKING_HRS=100;
              totalWorkingHrs += workingHrs; 
         System.out.println("Total Working Hours:" +totalWorkingHrs +    "Total Working Days:" +totalWorkingDays);
 
-      }
+       }
                 monthlyWage = totalWorkingHrs*wagePerHr;
-                System.out.println(" Monthly wage of Employee of "+company+" is: " +monthlyWage);
-
-    }
+     }
+     public String toString()
+   {
+       return   "Monthly wage of Employee of "+company+" is: " +monthlyWage;
+   }
 
    public static void main(String args[]) 
     {
     System.out.println("***Welcome to employee wage computation Program***");
-     EmpWage emp=new EmpWage();
-      emp.computeWage("Nauttica",20,90,20);
-      emp.computeWage("NothenKing",20,57,12);
-      emp.computeWage("AllStar007",20,67,19);
-  }
-}
+     EmpWage emp=new EmpWage("Nauttica",20,90,20);
+     EmpWage emp1=new EmpWage("AllStar",20,60,10);
+          emp.computeEmpWage();
+           System.out.println(emp);
+           emp1.computeEmpWage();
+           System.out.println(emp1);
+    }
+ }
