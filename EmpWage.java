bfd3e0373c95b67b5c535f9d942fb4b17219a3ff
rohits777 <rohitashinde95@gmail.com>
2020-09-25 14:13:42 +0530

    Refactor: the code using method

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,8 @@
 class EmpWage {
 public static final int IS_FULL_TIME=1,IS_PART_TIME=2,WAGE_PER_HR=20,WORKING_DAYS=20;
 static final int MAX_WORKING_HRS=100;
-public static void main(String[] args) {
+  public void computeWage()
+  {
      System.out.println("***Welcome to employee wage computation Program***");
 
      int workingHrs=0,dailyWage,monthlyWage=0,totalWorkingHrs=0,totalWorkingDays=0;
@@ -12,26 +13,32 @@ public static void main(String[] args) {
          switch(random) 
          {
            case IS_FULL_TIME:
-                 System.out.println("Employee Is Present full time ");
+                 
                  workingHrs=8;
                  break;
         
            case IS_PART_TIME:
-                 System.out.println("Employee Is present  part time .");
+                 
                  workingHrs=4;
                  break; 
 
             default:
-                System.out.println("Employee Is Absent ");
+                
                     workingHrs=0;
          }
 
              totalWorkingHrs += workingHrs; 
-            System.out.println("Total Working Hours:" +totalWorkingHrs + "Total Working Days:" +totalWorkingDays);
+        System.out.println("Total Working Hours:" +totalWorkingHrs +    "Total Working Days:" +totalWorkingDays);
 
       }
                 monthlyWage = totalWorkingHrs*WAGE_PER_HR;
                 System.out.println(" Monthly wage of Employee is " +monthlyWage);
 
     }
+
+   public static void main(String args[]) 
+    {
+     EmpWage emp=new EmpWage();
+      emp.computeWage();
+     }
 }
