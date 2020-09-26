41e94fed9f20dccb8f4db8b57e8ad265cf66bfca
rohits777 <rohitashinde95@gmail.com>
2020-09-26 14:01:52 +0530

    Added: employee wage builder for multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,64 +1,123 @@
-class EmpWage {
-public static final int IS_FULL_TIME=1,IS_PART_TIME=2;
-        private final String company;
-        private final int wagePerHr;
-        private final int workingDays;
-        private final int maxHrsPerMonth;
-        private int monthlyWage;
+import java.util.Scanner;
+import java.util.ArrayList;
 
+class Company
+{
 
-  public EmpWage (String company,int wagePerHr,int maxHrsPerMonth,int workingDays )
-  {
+        public final String company;
+        public final int wagePerHr;
+        public final int numbOfWorkingDays;
+        public final int numbWorkingHrs;
+
+        public Company(String company,int wagePerHr,int numbOfWorkingDays,int numbWorkingHrs)
+        {
                 this.company=company;
                 this.wagePerHr=wagePerHr;
-                this.workingDays=workingDays;
-                this.maxHrsPerMonth=maxHrsPerMonth;
-  }
-  public void computeEmpWage()
-  {
-     int workingHrs=0,dailyWage,monthlyWage,totalWorkingHrs=0,totalWorkingDays=0;
-      while (totalWorkingDays < workingDays && totalWorkingHrs <= maxHrsPerMonth)
-       {
-              totalWorkingDays++;
-               int random=(int)Math.floor(Math.random()*10)%3;
+                this.numbOfWorkingDays=numbOfWorkingDays;
+                this.numbWorkingHrs=numbWorkingHrs;
+        }
 
-          System.out.println(random);
-          switch (random)
-     {
-           case IS_FULL_TIME:
+}
 
-                 workingHrs=8;
-                 break;
+public class EmpWage
+{
 
-           case IS_PART_TIME:
+        public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
 
-                 workingHrs=4;
-                 break; 
+        public static void calculateEmpWage(Company company)
+        {
+        int currentWorkingHour=0;
+        int currentWorkingday=0;
+        int salary,workingHr=0;
+        int totalSalary=0;
 
-            default:
+        while( company.numbOfWorkingDays>=currentWorkingday && company.numbWorkingHrs>=currentWorkingHour )
+        {
+                int random=(int)Math.floor(Math.random()*10)%3;
+		System.out.println(random); 
+               switch(random)
+                {
+                        case FULL_TIME:
+                        workingHr=8;
+                        break;
 
-                    workingHrs=0;
+                        case PART_TIME:
+                        workingHr=4;
+                        break;
+
+                       default:
+                       workingHr=0;
+                 }
+                        currentWorkingday++;
+                        salary=workingHr*company.wagePerHr;
+                         totalSalary+=salary;
+        }
+                System.out.println("Total computed salary for "+company.company+" "+totalSalary);
+
+}
+
+        public static ArrayList readInputsFromUser()
+        {
+                ArrayList<Company> companyDetails = new ArrayList<Company>();
+                System.out.println("please enter number of companies");
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+
+                for(int i=0;i<n;i++)
+                {
+                        String company="";
+                        int workPerHr=0;
+                        int numbOfWorkingDays=0;
+                        int numbWorkingHrs=0;
+
+                        Scanner sc1=new Scanner(System.in);
+                        System.out.println("please enter company name:");
+                        company=sc1.nextLine();
+
+                        System.out.println("please enter workPerHr:");
+                        workPerHr=sc1.nextInt();
+
+                        System.out.println("please enter numberofworkingdays:");
+                        numbOfWorkingDays=sc1.nextInt();
+
+                        System.out.println("please enter numberofworkingHrs:");
+                        numbWorkingHrs=sc1.nextInt();
+
+                        Company com=new Company(company,workPerHr,numbOfWorkingDays,numbWorkingHrs);
+                        companyDetails.add(com);
+
+                }
+                return companyDetails;
+        }
+
+        public static void main(String[] args)
+        {
+                ArrayList<Company> array=readInputsFromUser();
+                Company c=null;
+                for(Company c2:array)
+                {
+                        String company="";
+                        int wagePerHr=0;
+                        int workingDays=0;
+                        int workingHrs=0;
+
+                        company=c2.company.toString();
+                        System.out.println("company "+company);
+
+                        wagePerHr=(int)c2.wagePerHr;
+                        System.out.println("wageperhr "+wagePerHr);
+
+                        workingDays=(int)c2.numbOfWorkingDays;
+                        System.out.println("workingDays "+workingDays);
+
+                        workingHrs=(int)c2.numbWorkingHrs;
+                        System.out.println("workingHrs "+workingHrs);
+
+                        c=new Company(company,wagePerHr,workingDays,workingHrs);
+                        calculateEmpWage(c);
+
+                }
+
          }
-
-             totalWorkingHrs += workingHrs; 
-        System.out.println("Total Working Hours:" +totalWorkingHrs +    "Total Working Days:" +totalWorkingDays);
-
-       }
-                monthlyWage = totalWorkingHrs*wagePerHr;
-     }
-     public String toString()
-   {
-       return   "Monthly wage of Employee of "+company+" is: " +monthlyWage;
-   }
-
-   public static void main(String args[]) 
-    {
-    System.out.println("***Welcome to employee wage computation Program***");
-     EmpWage emp=new EmpWage("Nauttica",20,90,20);
-     EmpWage emp1=new EmpWage("AllStar",20,60,10);
-          emp.computeEmpWage();
-           System.out.println(emp);
-           emp1.computeEmpWage();
-           System.out.println(emp1);
-    }
- }
+}
